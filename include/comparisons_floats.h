a18a895a1bdb40d7a98424fcd855a41324b313c8
Henry Malinowski <malinowski.henry@gmail.com>
2017-05-22 19:33:25 -0500

    added greater-than functions

--- include/comparisons_floats.h
+++ include/comparisons_floats.h
@@ -16,11 +16,11 @@ typedef int (*__compar_fn_t) (const void *, const void *);
  * @brief Compares two floating point values.
  * @details The return value from this function is in accordance with the
  *  pattern outlined for a comparison function used by qsort.
- * @param p, q Pointers to floats.
+ * @param p, q Pointers to floats to be compared.
  * @returns An int corresponding to the order of the values of \p p and \p q.
  * @retval +1: Indicates that \p p is strictly greater than \p q.
  * @retval -1: Indicates that \p p is strictly less than \p q.
- * @retval  0: Indicates that the value at \p p and \p q are equal.
+ * @retval  0: Indicates that the values at \p p and \p q are equal.
  * @see <a href="https://www.gnu.org/software/libc/manual/html_node/Comparison-Functions.html">
  * GCC libc Software Manuel | 9.1 Defining the Comparison Function</a>
  */
@@ -31,11 +31,11 @@ int cmp_less_fp32(const void* p, const void* q);
  * @brief Compares two double-precision floating point values.
  * @details The return value from this function is in accordance with the
  *  pattern outlined for a comparison function used by qsort.
- * @param p, q Pointers to floats.
+ * @param p, q Pointers to doubles to be compared.
  * @returns An int corresponding to the order of the values of \p p and \p q.
  * @retval +1: Indicates that \p p is strictly greater than \p q.
  * @retval -1: Indicates that \p p is strictly less than \p q.
- * @retval  0: Indicates that the value at \p p and \p q are equal.
+ * @retval  0: Indicates that the values at \p p and \p q are equal.
  * @see <a href="https://www.gnu.org/software/libc/manual/html_node/Comparison-Functions.html">
  * GCC libc Software Manuel | 9.1 Defining the Comparison Function</a>
  */
@@ -43,13 +43,13 @@ int cmp_less_fp64(const void* p, const void* q);
 
 
 /**
- * @brief Compares two 'long-double' precision floating point values.
+ * @brief Compares two 'long-double' floating point values.
  * @details The return value from this function is in accordance with the
  *  pattern outlined for a comparison function used by qsort. The exact
  *  precision of this function is platform and compiler dependant, on some
  *  platforms this may be an 80-bit, 96-bit, or even an 128-bit floating point
  *  value.
- * @param p, q Pointers to floats.
+ * @param p, q Pointers to long-doubles to be compared.
  * @returns An int corresponding to the order of the values of \p p and \p q.
  * @retval +1: Indicates that \p p is strictly greater than \p q.
  * @retval -1: Indicates that \p p is strictly less than \p q.
@@ -58,5 +58,54 @@ int cmp_less_fp64(const void* p, const void* q);
  * GCC libc Software Manuel | 9.1 Defining the Comparison Function</a>
  */
 int cmp_less_fp128(const void *p, const void *q);
+
+
+/**
+ * @brief Compares two floating point values.
+ * @details The return value from this function is in accordance with the
+ *  pattern outlined for a comparison function used by qsort.
+ * @param p, q Pointers to doubles to be compared.
+ * @returns An int corresponding to the order of the values of \p p and \p q.
+ * @retval +1: Indicates that \p q is strictly greater than \p p.
+ * @retval -1: Indicates that \p q is strictly less than \p p.
+ * @retval  0: Indicates that the values at \p p and \p q are equal.
+ * @see <a href="https://www.gnu.org/software/libc/manual/html_node/Comparison-Functions.html">
+ * GCC libc Software Manuel | 9.1 Defining the Comparison Function</a>
+ */
+int cmp_great_fp32(const void* p, const void* q);
+
+
+/**
+ * @brief Compares two double-precision floating point values.
+ * @details The return value from this function is in accordance with the
+ *  pattern outlined for a comparison function used by qsort.
+ * @param p, q Pointers to doubles to be compared.
+ * @returns An int corresponding to the order of the values of \p p and \p q.
+ * @retval +1: Indicates that \p q is strictly greater than \p p.
+ * @retval -1: Indicates that \p q is strictly less than \p p.
+ * @retval  0: Indicates that the values at \p p and \p q are equal.
+ * @see <a href="https://www.gnu.org/software/libc/manual/html_node/Comparison-Functions.html">
+ * GCC libc Software Manuel | 9.1 Defining the Comparison Function</a>
+ */
+int cmp_great_fp64(const void* p, const void* q);
+
+
+/**
+ * @brief Compares two 'long-double' floating point values.
+ * @details The return value from this function is in accordance with the
+ *  pattern outlined for a comparison function used by qsort. The exact
+ *  precision of this function is platform and compiler dependant, on some
+ *  platforms this may be an 80-bit, 96-bit, or even an 128-bit floating point
+ *  value.
+ * @param p, q Pointers to long-doubles to be compared.
+ * @returns An int corresponding to the order of the values of \p p and \p q.
+ * @retval +1: Indicates that \p q is strictly greater than \p p.
+ * @retval -1: Indicates that \p q is strictly less than \p p.
+ * @retval  0: Indicates that the values at \p p and \p q are equal.
+ * @see <a href="https://www.gnu.org/software/libc/manual/html_node/Comparison-Functions.html">
+ * GCC libc Software Manuel | 9.1 Defining the Comparison Function</a>
+ */
+int cmp_great_fp128(const void* p, const void* q);
+
 
 #endif //COMPARISONS_FLOATS_H
--- src/comparisons_floats.c
+++ src/comparisons_floats.c
@@ -1,5 +1,6 @@
 #include "../include/comparisons_floats.h"
 
+/* Less than functions */
 int
 cmp_less_fp32(const void* p, const void* q)
 {
@@ -32,5 +33,41 @@ cmp_less_fp128(const void *p, const void *q)
 
     if (a > b) return +1;
     if (b > a) return -1;
+    return 0;
+}
+
+/* Greater-then functions */
+int
+cmp_great_fp32(const void* p, const void* q)
+{
+    const float a = *(const float*) p;
+    const float b = *(const float*) q;
+
+    if (b > a) return +1;
+    if (a > b) return -1;
+    return 0;
+}
+
+
+int
+cmp_great_fp64(const void* p, const void* q)
+{
+    const double a = *(const double *) p;
+    const double b = *(const double *) q;
+
+    if (b > a) return +1;
+    if (a > b) return -1;
+    return 0;
+}
+
+
+int
+cmp_great_fp128(const void *p, const void *q)
+{
+    const long double a = *(const long double *) p;
+    const long double b = *(const long double *) q;
+
+    if (b > a) return +1;
+    if (a > b) return -1;
     return 0;
 }
